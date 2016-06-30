4558fb4fe9302a99644717fb89747f36800fd258
yondop <yondop@gmail.com>
2016-07-01 02:19:30 +0500

    init

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>This is template app</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
