f9cfab434a9f65d508c61334d4546a012f8e6a93
yondop <yondop@gmail.com>
2016-07-01 15:48:52 +0500

    consts

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,9 @@
+import * as types from '../actions/actionTypes';
+
+
 export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case 'CREATE_COURSE':
+    case types.CREATE_COURSE:
       return [...state,
         Object.assign({}, action.course)
       ];
